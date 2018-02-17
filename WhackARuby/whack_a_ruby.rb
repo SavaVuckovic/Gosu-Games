f97fa5083a1539794c63f4c7b9a4dc906e641206
Sava <vuckovicsava11@gmail.com>
2018-02-17 11:53:48 +0100

    drawing the hammer on mouse position

--- WhackARuby/whack_a_ruby.rb
+++ WhackARuby/whack_a_ruby.rb
@@ -9,12 +9,14 @@ class WhackARuby < Gosu::Window
     super(SCREEN_WIDTH, SCREEN_HEIGHT)
     self.caption = 'Whack a Ruby!'
     @ruby = Gosu::Image.new('ruby.png')
+    @hammer = Gosu::Image.new('hammer.png')
     @x = 300
     @y = 300
     @width = 50
     @height = 43
     @velocity_x = 5
     @velocity_y = 5
+    @visible = 0
   end
 
   def update
@@ -23,10 +25,16 @@ class WhackARuby < Gosu::Window
 
     @velocity_x *= -1 if @x + @width/2 > 800 || @x - @width/2 < 0
     @velocity_y *= -1 if @y + @height/2 > 600 || @y - @height/2 < 0
+
+    @visible -= 1
+    @visible = 30 if @visible < -10 && rand < 0.01
   end
 
   def draw
-    @ruby.draw(@x - @width/2, @y - @height/2, 1)
+    if @visible > 0
+      @ruby.draw(@x - @width/2, @y - @height/2, 1)
+    end
+    @hammer.draw(mouse_x - 25, mouse_y - 25, 1)
   end
 
 end
