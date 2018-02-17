716e94d3aad9284f1b5117ec16e6f8e0930b3d1c
Sava <vuckovicsava11@gmail.com>
2018-02-17 12:04:57 +0100

    this is starting to look like a game

--- WhackARuby/whack_a_ruby.rb
+++ WhackARuby/whack_a_ruby.rb
@@ -17,6 +17,17 @@ class WhackARuby < Gosu::Window
     @velocity_x = 5
     @velocity_y = 5
     @visible = 0
+    @hit = 0
+  end
+
+  def button_down(id)
+    if id == Gosu::MsLeft
+      if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
+        @hit = 1
+      else
+        @hit = -1
+      end
+    end
   end
 
   def update
@@ -27,14 +38,25 @@ class WhackARuby < Gosu::Window
     @velocity_y *= -1 if @y + @height/2 > 600 || @y - @height/2 < 0
 
     @visible -= 1
-    @visible = 30 if @visible < -10 && rand < 0.01
+    @visible = 50 if @visible < -10 && rand < 0.01
   end
 
   def draw
     if @visible > 0
       @ruby.draw(@x - @width/2, @y - @height/2, 1)
     end
+
     @hammer.draw(mouse_x - 25, mouse_y - 25, 1)
+
+    if @hit == 0
+      c = Gosu::Color::NONE
+    elsif @hit == 1
+      c = Gosu::Color::GREEN
+    elsif @hit == -1
+      c = Gosu::Color::RED
+    end
+    draw_quad(0, 0, c, 800, 0, c, 800, 600, c, 0, 600, c)
+    @hit = 0
   end
 
 end
