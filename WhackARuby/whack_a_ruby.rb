9ffc12c65e000de996163a1a729442bc277b3b0d
Sava <vuckovicsava11@gmail.com>
2018-02-17 12:45:48 +0100

    Whack a Ruby game complete :)

--- WhackARuby/whack_a_ruby.rb
+++ WhackARuby/whack_a_ruby.rb
@@ -8,6 +8,7 @@ class WhackARuby < Gosu::Window
   def initialize
     super(SCREEN_WIDTH, SCREEN_HEIGHT)
     self.caption = 'Whack a Ruby!'
+    @font = Gosu::Font.new(30)
     @ruby = Gosu::Image.new('ruby.png')
     @hammer = Gosu::Image.new('hammer.png')
     @x = 300
@@ -18,30 +19,55 @@ class WhackARuby < Gosu::Window
     @velocity_y = 5
     @visible = 0
     @hit = 0
+    @score = 0
+    @playing = true
+    @start_time = 0
   end
 
   def button_down(id)
-    if id == Gosu::MsLeft
-      if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
-        @hit = 1
-      else
-        @hit = -1
+    if @playing
+      if id == Gosu::MsLeft
+        if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
+          @hit = 1
+          @score += 5
+        else
+          @hit = -1
+          @score -= 1
+        end
+      end
+    else
+      if id == Gosu::KbSpace
+        @playing = true
+        @visible = -10
+        @start_time = Gosu.milliseconds
+        @score = 0
       end
     end
   end
 
   def update
-    @x += @velocity_x
-    @y += @velocity_y
+    if @playing
+      @x += @velocity_x
+      @y += @velocity_y
 
-    @velocity_x *= -1 if @x + @width/2 > 800 || @x - @width/2 < 0
-    @velocity_y *= -1 if @y + @height/2 > 600 || @y - @height/2 < 0
+      @velocity_x *= -1 if @x + @width/2 > 800 || @x - @width/2 < 0
+      @velocity_y *= -1 if @y + @height/2 > 600 || @y - @height/2 < 0
 
-    @visible -= 1
-    @visible = 50 if @visible < -10 && rand < 0.01
+      @visible -= 1
+      @visible = 50 if @visible < -10 && rand < 0.01
+
+      @time_left = (100 - (Gosu.milliseconds - @start_time) / 1000)
+      @playing = false if @time_left <= 0
+    end
   end
 
   def draw
+    unless @playing
+      @font.draw('Game Over', 300, 300, 3)
+      @font.draw('Press the Space Bar to Play Again', 175, 350, 3)
+      @visible = 20
+    end
+
     if @visible > 0
       @ruby.draw(@x - @width/2, @y - @height/2, 1)
     end
@@ -57,6 +83,9 @@ class WhackARuby < Gosu::Window
     end
     draw_quad(0, 0, c, 800, 0, c, 800, 600, c, 0, 600, c)
     @hit = 0
+
+    @font.draw(@score.to_s, 700, 20, 2)
+    @font.draw(@time_left.to_s, 20, 20, 2)
   end
 
 end
