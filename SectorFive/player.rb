5cc1e2cb214fa7e32b50f314dfed4b96417ad2ed
Sava <vuckovicsava11@gmail.com>
2018-02-18 11:26:38 +0100

    ship movement implemented

--- SectorFive/player.rb
+++ SectorFive/player.rb
@@ -1,14 +1,40 @@
 class Player
 
+  ROTATION_SPEED = 3
+  ACCELERATION = 1.5
+  FRICTION = 0.9
+
   def initialize(window)
     @x = 200
     @y = 200
+    @velocity_x = 0
+    @velocity_y = 0
     @angle = 0
     @image = Gosu::Image.new('images/spaceship.png')
   end
 
   def draw
     @image.draw_rot(@x, @y, 1, @angle)
+  end
+
+  def turn_right
+    @angle += ROTATION_SPEED
+  end
+
+  def turn_left
+    @angle -= ROTATION_SPEED
+  end
+
+  def move
+    @x += @velocity_x
+    @y += @velocity_y
+    @velocity_x *= FRICTION
+    @velocity_y *= FRICTION
+  end
+
+  def accelerate
+    @velocity_x += Gosu.offset_x(@angle, ACCELERATION)
+    @velocity_y += Gosu.offset_y(@angle, ACCELERATION)
   end
 
 end
--- SectorFive/sector_five.rb
+++ SectorFive/sector_five.rb
@@ -13,6 +13,13 @@ class SectorFive < Gosu::Window
     @player = Player.new(self)
   end
 
+  def update
+    @player.turn_left if button_down?(Gosu::KbLeft)
+    @player.turn_right if button_down?(Gosu::KbRight)
+    @player.accelerate if button_down?(Gosu::KbUp)
+    @player.move
+  end
+
   def draw
     @player.draw
   end
