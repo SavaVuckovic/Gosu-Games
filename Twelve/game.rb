8ba23842052fa8705d9f5ed81c531afdd5038fa9
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-04-07 16:32:26 +0200

    positioning ficed, added comments

--- Twelve/game.rb
+++ Twelve/game.rb
@@ -7,12 +7,14 @@ class Game
     @squares = []
     color_list = []
 
+    # push each color 12 times in the color_list array
     [:red, :green, :blue].each do |color|
-      12.times do 
+      12.times do
         color_list.push color
       end
     end
 
+    # shiffle colors and create a grid
     color_list.shuffle!
     (0..5).each do |row|
       (0..5).each do |column|
@@ -24,7 +26,7 @@ class Game
     @font = Gosu::Font.new(36)
   end
 
-  def draw 
+  def draw
     @squares.each do |square|
       square.draw
     end
--- Twelve/square.rb
+++ Twelve/square.rb
@@ -13,7 +13,7 @@ class Square
 
     @@font ||= Gosu::Font.new(72)
     @@window ||= window
-    
+
     @row = row
     @column = column
     @color = color
@@ -38,9 +38,9 @@ class Square
       @@window.draw_quad(x1, y1, c, x2, y2, c, x3, y3, c, x4, y4, c, 2)
 
       x_center = x1 + 48
-      x_text = x_center - @@font.text_width("#{@number} ") / 2
-      y_text = y1 + 12
-      @@font.draw("#{@number} ",x_text,y_text,1)
+      x_text = x_center - @@font.text_width(" #{@number} ") / 2
+      y_text = y1 + 22
+      @@font.draw(" #{@number} ", x_text, y_text, 1)
     end
   end
 
